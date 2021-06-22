9186aa00e06ee34a46d27b56cca7e656fdb40c8c
George Seah <shanjin14@users.noreply.github.com>
2021-06-22 18:22:07 +0800

    Add files via upload

--- P5 - Airflow/plugins/operators/data_quality.py
+++ P5 - Airflow/plugins/operators/data_quality.py
@@ -10,19 +10,23 @@ class DataQualityOperator(BaseOperator):
     def __init__(self,
                  redshift_conn_id="",
                  tablelist ="",
+                 dq_checks="",
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.tablelist = tablelist
+        self.dq_checks=dq_checks
 
     def execute(self, context):
         redshift_hook = PostgresHook(self.redshift_conn_id)
-        for tbl in  self.tablelist:   
-            records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {tbl}")
-            if len(records) < 1 or len(records[0]) < 1:
-                raise ValueError(f"Data quality check failed. {tbl} returned no results")
-            num_records = records[0][0]
-            if num_records < 1:
-                raise ValueError(f"Data quality check failed. {tbl} contained 0 rows")
-            self.log.info(f"Data quality on table {tbl} check passed with {records[0][0]} records")
+        for chk in  self.dq_checks:
+            qry=chk['sql']
+            result=chk['expected_result']
+            checkdesc=chk['description']
+            records = redshift_hook.get_records(qry)
+            self.log.info(f"expected result {result} compare with check result {records[0][0]}")
+            if abs(result-records[0][0])>0.001:
+                raise ValueError(f"Data quality check failed. {qry} {checkdesc} failed")
+            self.log.info(f"Data quality on table {qry} {checkdesc} check passed")
+            
--- P5 - Airflow/plugins/operators/load_dimension.py
+++ P5 - Airflow/plugins/operators/load_dimension.py
@@ -10,15 +10,25 @@ class LoadDimensionOperator(BaseOperator):
     def __init__(self,
                  redshift_conn_id="",
                  query="",
+                 append_data =False,
+                 table_name ="",
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.query = query
+        self.append_data = append_data
+        self.table_name = table_name
 
     def execute(self, context):
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
         self.log.info("Connect to RedShift")
-        redshift.run(self.query)
-        self.log.info('Run %s', self.query)
+        if self.append_data:
+            redshift.run(self.query)
+            self.log.info('Run Append Load %s', self.query)
+        else:
+            truncate_statement = 'TRUNCATE %s' % self.table_name
+            redshift.run(truncate_statement)
+            redshift.run(self.query)
+            self.log.info('Run Truncate and Load %s %s',truncate_statement, self.query)
 
